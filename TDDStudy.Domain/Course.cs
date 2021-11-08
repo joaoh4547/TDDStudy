dc6781f7e9bea9d11df4234efe7ea2aa662ad22e
João Henrique <joaohenriquetdj@gmail.com>
2021-11-08 20:33:33 -0300

    add course entity

---
+++ TDDStudy.Domain/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDStudy.Domain
+{
+    public class Course
+    {
+        public string Name { get; private set; }
+        public double Workload { get; set; }
+        public string TargetAdience { get; set; }
+        public decimal Value { get; set; }
+
+        public Course(string name, double workload, string targetAdience, decimal value)
+        {
+            Name = name;
+            Workload = workload;
+            TargetAdience = targetAdience;
+            Value = value;
+        }
+
+    }
+}
